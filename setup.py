16b9e252b8a98cdf24a445ded680dd42fff8d0bd
Maarten Thibaut <mthibaut@cisco.com>
2014-12-14 15:25:37 +0100

    Fixed requires in setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@
 from distutils.core import setup
 
 setup(name='wait-ios-serial',
-      version='1.0',
+      version='1.0.2',
       url='https://github.com/mthibaut/wait-ios-serial',
       description='Wait for a Cisco IOS device to come up from boot',
       author='Maarten Thibaut',
       author_email='mthibaut@cisco.com',
-      scripts=['scripts/wait-ios-serial']
+      scripts=['scripts/wait-ios-serial'],
+      install_requires=['pexpect']
 )
 
 
