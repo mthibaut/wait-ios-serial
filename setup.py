0c38bfc7450d02eacc756c476b9b7006d6616562
Maarten Thibaut <mthibaut@cisco.com>
2014-12-14 15:11:44 +0100

    First version, uploaded as 1.0.0 to pypi.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wait-ios-serial',
+      version='1.0',
+      url='https://github.com/mthibaut/wait-ios-serial',
+      description='Wait for a Cisco IOS device to come up from boot',
+      author='Maarten Thibaut',
+      author_email='mthibaut@cisco.com',
+      scripts=['scripts/wait-ios-serial']
+)
+
+
+
