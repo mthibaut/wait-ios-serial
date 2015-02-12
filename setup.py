fcbc62528d232215ff72aaf009cc1d467bd13568
Maarten Thibaut <mthibaut@cisco.com>
2015-02-12 14:20:28 +0100

    Adding wait-ios-login to setup script. Bumping release.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='wait-ios-serial',
-      version='1.0.5',
+      version='1.0.6',
       url='https://github.com/mthibaut/wait-ios-serial',
       description='Wait for a Cisco IOS device to come up from boot',
       author='Maarten Thibaut',
       author_email='mthibaut@cisco.com',
-      scripts=['scripts/wait-ios-serial'],
+      scripts=['scripts/wait-ios-serial', 'scripts/wait-ios-login'],
       install_requires=['pexpect']
 )
 
