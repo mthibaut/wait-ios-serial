34b8a78d5a71d7aabf28762d0c3fc90b85603da7
Maarten Thibaut <mthibaut@cisco.com>
2015-02-12 15:27:46 +0100

    Bumping release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='wait-ios-serial',
-      version='1.0.6',
+      version='1.0.7',
       url='https://github.com/mthibaut/wait-ios-serial',
       description='Wait for a Cisco IOS device to come up from boot',
       author='Maarten Thibaut',
